a23467fd4e5d1dd627bbdf9779170972b73d428b
Heem <42186537+HeemPlayz@users.noreply.github.com>
2020-06-04 19:20:51 +0400

    Update student.java

--- src/Test/student.java
+++ src/Test/student.java
@@ -2,10 +2,10 @@ package Test;
 
 public class student {
 
-	String firstName = "Ibrahim";
-	String lastName = "Eyad";
-	int Age = 16;
-	String Email = "brro2004@gmail.com";		
+	String firstName = "";
+	String lastName = "";
+	int Age = ;
+	String Email = "";		
 	
 	String getStudentInfo() {
 		return(firstName + " " +  lastName);
